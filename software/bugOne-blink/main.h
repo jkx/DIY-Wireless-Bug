fabc8743908325e727780877eb27785dff43ca4a
Jkx <jkx@larsen-b.com>
2011-07-27 23:36:24 +0200

    Initials tests for bugOne.

---
+++ software/bugOne-blink/main.h
@@ -0,0 +1,19 @@
+
+
+#ifndef _main_h_
+#define _main_h_
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/power.h>
+
+
+
+#include "avr_compat.h"
+
+#define BAUD 115200L          // Baudrate
+#define F_CPU 8000000        /* Oszillator-Frequenz in Hz */
+
+
+#endif
