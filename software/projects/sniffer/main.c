9f3e84d7cf7302a5eeff34729621447dd8ffb16d
Nicolas DUPEUX <nicolas@dupeux.net>
2011-10-23 16:22:32 +0200

    Add ping command on sniffer

--- software/projects/sniffer/main.c
+++ software/projects/sniffer/main.c
@@ -5,10 +5,32 @@
 #include <util/delay.h>
 #include <stdlib.h>
 
+#include "config.h"
 #include "rfm12_config.h"
 #include "rfm12.h"
 #include "uart.h"
 #include "avr_compat.h"
+
+/* Ping command. To ping destination address 25 :
+   P25 
+ */
+void run_cmd_ping(char *params) {
+	uint8_t dst;
+	
+	dst=atoi(params);
+
+	send_ping(dst);
+}
+
+void run_cmd(char *cmd) {
+	switch (cmd[0]) {
+		case 'P':
+		case 'p':
+			cmd+=1;
+			run_cmd_ping(cmd);
+			break;
+	}
+}
 
 int main ( void )
 {
@@ -17,6 +39,8 @@ int main ( void )
   uint16_t packet_count = 0;
   uint8_t len;
   char strbuf[6];
+  char cmd[10];
+  char *c;
 	
   drive(LED1);
   drive(LED2);
@@ -25,6 +49,17 @@ int main ( void )
   toggle_output(LED2);
 	
   uart_init();
+
+  uart_putstr_P(PSTR("Firmware version "));
+  uart_putstr_P(PSTR(FWVERSION));
+  uart_putstr_P(PSTR("\r\n"));
+
+  config_init();
+
+  uart_putstr_P(PSTR("Node address : "));
+  itoa(config.address,strbuf,10);
+  uart_putstr(strbuf);
+  uart_putstr_P(PSTR("\r\n"));
 
   _delay_ms(250);
   _delay_ms(250);
@@ -60,13 +95,22 @@ int main ( void )
           uart_hexdump((char*)bufcontents,len);
 	  rfm12_rx_clear();
 	}
-      _delay_ms(5);
 
       if (ticker > 50000) {
           ticker = 0;
           toggle_output(LED2);
       }
-      
+
+	  if (uart_getc_nb(c) == 1) {
+	    if (*c == 13) {
+		  *c = 0;
+		  run_cmd(cmd);
+		  c = cmd;
+		} else if ((c - cmd) < sizeof(cmd)) {
+	  	  c++; // ahah
+		}
+	  }
+
       rfm12_tick();
   }
 }
