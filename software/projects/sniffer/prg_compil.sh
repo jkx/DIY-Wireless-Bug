f78d245fad6813ce5d39894a0481bbaadfe1411d
Jkx <jkx@larsen-b.com>
2011-11-20 18:24:50 +0100

    Deleted unused scripts

--- software/projects/sniffer/prg_compil.sh
+++
@@ -1,9 +0,0 @@
-$CC $CFLAGS -c ../../core/protocol/network.c -o $BOARD/network.o
-$CC $CFLAGS -c ../../core/protocol/ping.c -o $BOARD/ping.o
-$CC $CFLAGS -c ../../core/protocol/get.c -o $BOARD/get.o
-$CC $CFLAGS -c ../../core//apps.c -o $BOARD/app.o
-$CC $CFLAGS -c ../../core/protocol/value.c -o $BOARD/value.o
-$CC $CFLAGS -c ../../core/config.c -o $BOARD/config.o
-$CC $CFLAGS -c ../../core/uart/uart.c -o $BOARD/uart.o
-$CC $CFLAGS -c ../../core/rfm12.c -o $BOARD/rfm12.o
-$CC $CFLAGS -c main.c -o $BOARD/main.o
