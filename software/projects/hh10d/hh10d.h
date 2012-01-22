58f1c7a54e3aa37074c7ea03b8ee30ad338ce78c
Nicolas DUPEUX <nicolas@dupeux.net>
2012-01-22 23:48:32 +0100

    Add hh10d project

---
+++ software/projects/hh10d/hh10d.h
@@ -0,0 +1,10 @@
+#ifndef HH10D_H
+#define HH10D_H
+
+#include "apps.h"
+
+void hh10d_init(void*);
+void hh10d_freq(void);
+int8_t hh10d_read(struct data_t *data);
+
+#endif
---
+++ software/projects/hh10d/twi.c
@@ -0,0 +1,235 @@
+#include "board.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <avr/sleep.h>
+#include <util/twi.h>
+
+#include "uart.h"
+
+/*
+ * Note [3]
+ * TWI address for 24Cxx EEPROM:
+ *
+ * 1 0 1 0 E2 E1 E0 R/~W	24C01/24C02
+ * 1 0 1 0 E2 E1 A8 R/~W	24C04
+ * 1 0 1 0 E2 A9 A8 R/~W	24C08
+ * 1 0 1 0 A10 A9 A8 R/~W	24C16
+ */
+#define TWI_SLA_24CXX	0xA2	/* E2 E1 E0 = 0 0 1 */
+
+/*
+ * Maximal number of iterations to wait for a device to respond for a
+ * selection.  Should be large enough to allow for a pending write to
+ * complete, but low enough to properly abort an infinite loop in case
+ * a slave is broken or not present at all.  With 100 kHz TWI clock,
+ * transfering the start condition and SLA+R/W packet takes about 10
+ * µs.  The longest write period is supposed to not exceed ~ 10 ms.
+ * Thus, normal operation should not require more than 100 iterations
+ * to get the device to respond to a selection.
+ */
+#define MAX_ITER	200
+
+uint8_t twst;
+
+void twi_init()
+{
+	// We use external pull-up on TWI bus
+	//PORTC = (1 << 5) | (1 << 4);
+
+	// Prescaler = 1
+	TWSR = 0;
+	// Bitrate (100 kHz)
+	TWBR = (F_CPU / 100000UL - 16) / 2;
+}
+
+/*
+ * Note [7]
+ *
+ * Read "len" bytes from EEPROM starting at "eeaddr" into "buf".
+ *
+ * This requires two bus cycles: during the first cycle, the device
+ * will be selected (master transmitter mode), and the address
+ * transfered.
+ * Address bits exceeding 256 are transfered in the
+ * E2/E1/E0 bits (subaddress bits) of the device selector.
+ * Address is sent in two dedicated 8 bit transfers
+ * for 16 bit address devices (larger EEPROM devices)
+ *
+ * The second bus cycle will reselect the device (repeated start
+ * condition, going into master receiver mode), and transfer the data
+ * from the device to the TWI master.  Multiple bytes can be
+ * transfered by ACKing the client's transfer.  The last transfer will
+ * be NACKed, which the client will take as an indication to not
+ * initiate further transfers.
+ */
+int
+ee24xx_read_bytes(uint16_t eeaddr, int len, uint8_t *buf)
+{
+  uint8_t sla, twcr, n = 0;
+  int rv = 0;
+
+
+#ifndef WORD_ADDRESS_16BIT
+  /* patch high bits of EEPROM address into SLA */
+  sla = TWI_SLA_24CXX | (((eeaddr >> 8) & 0x07) << 1);
+#else
+  /* 16-bit address devices need only TWI Device Address */
+  sla = TWI_SLA_24CXX;
+#endif
+
+  /*
+   * Note [8]
+   * First cycle: master transmitter mode
+   */
+  restart:
+  if (n++ >= MAX_ITER)
+    return -1;
+  begin:
+
+  TWCR = _BV(TWINT) | _BV(TWSTA) | _BV(TWEN); /* send start condition */
+  while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+  switch ((twst = TW_STATUS))
+    {
+    case TW_REP_START:		/* OK, but should not happen */
+    case TW_START:
+      break;
+
+    case TW_MT_ARB_LOST:	/* Note [9] */
+      goto begin;
+
+    default:
+      return -2;		/* error: not in start condition */
+				/* NB: do /not/ send stop condition */
+    }
+
+  /* Note [10] */
+  /* send SLA+W */
+  TWDR = sla | TW_WRITE;
+  TWCR = _BV(TWINT) | _BV(TWEN); /* clear interrupt to start transmission */
+  while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+  switch ((twst = TW_STATUS))
+    {
+    case TW_MT_SLA_ACK:
+      break;
+
+    case TW_MT_SLA_NACK:	/* nack during select: device busy writing */
+				/* Note [11] */
+      goto restart;
+
+    case TW_MT_ARB_LOST:	/* re-arbitrate */
+      goto begin;
+
+    default:
+      goto error;		/* must send stop condition */
+    }
+
+#ifdef WORD_ADDRESS_16BIT
+  TWDR = (eeaddr >> 8);		/* 16-bit word address device, send high 8 bits of addr */
+  TWCR = _BV(TWINT) | _BV(TWEN); /* clear interrupt to start transmission */
+  while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+  switch ((twst = TW_STATUS))
+    {
+    case TW_MT_DATA_ACK:
+      break;
+
+    case TW_MT_DATA_NACK:
+      goto quit;
+
+    case TW_MT_ARB_LOST:
+      goto begin;
+
+    default:
+      goto error;		/* must send stop condition */
+    }
+#endif
+
+  TWDR = eeaddr;		/* low 8 bits of addr */
+  TWCR = _BV(TWINT) | _BV(TWEN); /* clear interrupt to start transmission */
+  while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+  switch ((twst = TW_STATUS))
+    {
+    case TW_MT_DATA_ACK:
+      break;
+
+    case TW_MT_DATA_NACK:
+      goto quit;
+
+    case TW_MT_ARB_LOST:
+      goto begin;
+
+    default:
+      goto error;		/* must send stop condition */
+    }
+
+  /*
+   * Note [12]
+   * Next cycle(s): master receiver mode
+   */
+  TWCR = _BV(TWINT) | _BV(TWSTA) | _BV(TWEN); /* send (rep.) start condition */
+  while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+  switch ((twst = TW_STATUS))
+    {
+    case TW_START:		/* OK, but should not happen */
+    case TW_REP_START:
+      break;
+
+    case TW_MT_ARB_LOST:
+      goto begin;
+
+    default:
+      goto error;
+    }
+
+  /* send SLA+R */
+  TWDR = sla | TW_READ;
+  TWCR = _BV(TWINT) | _BV(TWEN); /* clear interrupt to start transmission */
+  while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+  switch ((twst = TW_STATUS))
+    {
+    case TW_MR_SLA_ACK:
+      break;
+
+    case TW_MR_SLA_NACK:
+      goto quit;
+
+    case TW_MR_ARB_LOST:
+      goto begin;
+
+    default:
+      goto error;
+    }
+
+  for (twcr = _BV(TWINT) | _BV(TWEN) | _BV(TWEA) /* Note [13] */;
+       len > 0;
+       len--)
+    {
+      if (len == 1)
+	twcr = _BV(TWINT) | _BV(TWEN); /* send NAK this time */
+      TWCR = twcr;		/* clear int to start transmission */
+      while ((TWCR & _BV(TWINT)) == 0) ; /* wait for transmission */
+      switch ((twst = TW_STATUS))
+	{
+	case TW_MR_DATA_NACK:
+	  len = 0;		/* force end of loop */
+	  /* FALLTHROUGH */
+	case TW_MR_DATA_ACK:
+	  *buf++ = TWDR;
+	  rv++;
+	  break;
+
+	default:
+	  goto error;
+	}
+    }
+  quit:
+  /* Note [14] */
+  TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN); /* send stop condition */
+
+  return rv;
+
+  error:
+  rv = -3;
+  goto quit;
+}
