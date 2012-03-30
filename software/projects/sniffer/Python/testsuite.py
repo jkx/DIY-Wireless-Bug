ba742c87aadb6b725dafa3e5e3b566596f41ef3e
Nicolas DUPEUX <nicolas@dupeux.net>
2012-03-30 22:40:49 +0200

    Replace device with node in testsuite

--- software/projects/sniffer/Python/testsuite.py
+++ software/projects/sniffer/Python/testsuite.py
@@ -10,10 +10,10 @@ import bugOne
 # 
 # Send a ping, wait for a pong
 #
-def test_ping(sniffer, deviceid):
+def test_ping(sniffer, nodeid):
 	print "ping"
 
-	bugOne.ping(destDeviceId, sniffer)
+	bugOne.ping(nodeid, sniffer)
 	message = sniffer.waitForMessage()
 
 	if message and bugOne.getPacketType(message) == bugOne.PACKET_PONG:
@@ -23,9 +23,9 @@ def test_ping(sniffer, deviceid):
 #
 # Turn on led 1 on tested bugone
 #
-def test_set_led1(sniffer, deviceid):
+def test_set_led1(sniffer, nodeid):
 	print "Turn on led 1"
-	bugOne.setValue(deviceid, 0, 1, value=1, sniffer=sniffer)
+	bugOne.setValue(nodeid, 0, 1, value=1, sniffer=sniffer)
 
 	while True:
 		answer = raw_input("led1 in on ? (y/n) ")
@@ -38,9 +38,9 @@ def test_set_led1(sniffer, deviceid):
 #
 # Turn off led 1 on tested bugone
 #
-def test_clr_led1(sniffer, deviceid):
+def test_clr_led1(sniffer, nodeid):
 	print "Turn off led 1"
-	bugOne.setValue(deviceid, 0, 1, value=0, sniffer=sniffer)
+	bugOne.setValue(nodeid, 0, 1, value=0, sniffer=sniffer)
 
 	while True:
 		answer = raw_input("led1 in off ? (y/n) ")
@@ -53,9 +53,9 @@ def test_clr_led1(sniffer, deviceid):
 #
 # Get value of the 1st device, should be 42 (const device)
 #
-def test_get_const(sniffer, deviceid):
+def test_get_const(sniffer, nodeid):
 	print "Get value device 1"
-	value = bugOne.getValue(deviceid, 0, 0, sniffer)
+	value = bugOne.getValue(nodeid, 0, 0, sniffer)
 	print "value is %s" % value
 
 	if value == 42:
@@ -65,14 +65,21 @@ def test_get_const(sniffer, deviceid):
 #
 # Get state of the led
 #
-def test_get_led1(sniffer, deviceid):
+def test_get_led1(sniffer, nodeid):
 	print "Get state led 1"
-	value = bugOne.getValue(deviceid, 0, 1, sniffer)
+	value = bugOne.getValue(nodeid, 0, 1, sniffer)
 	print "state is %s" % value
 
 	if value == 1:
 		return True
 	return False
+
+#
+# Get state of an undeclared device
+#
+def test_get_unknown_device(sniffer, nodeid):
+    print "Get state of unknown device"
+    return False
 
 print """
 bugOne test suite.
@@ -81,13 +88,13 @@ bugOne test suite.
 """
 
 if len(sys.argv) != 2:
-	print >>sys.stderr, "usage:", sys.argv[0], "<device id (int)>"
+	print >>sys.stderr, "usage:", sys.argv[0], "<node id (int)>"
 	exit(1)
 
 sniffer = rfm12.SnifferSerial()
-destDeviceId = int(sys.argv[1])
+destNodeId = int(sys.argv[1])
 
-print "Running testsuite with device %s" % destDeviceId
+print "Running testsuite with node %s" % destNodeId
 
 tests = [ test_ping, test_get_const, test_set_led1, test_get_led1, test_clr_led1 ]
 nb_tests_ok=0
@@ -95,7 +102,7 @@ nb_tests_ko=0
 
 for test in tests:
 	print "***" ,
-	if test(sniffer,destDeviceId):
+	if test(sniffer,destNodeId):
 		nb_tests_ok+=1
 		print "===> OK"
 	else:
