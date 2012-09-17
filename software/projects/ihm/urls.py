ad3851d919b90685b05ce796d554b52e9e53bb7a
Nicolas Dupeux <nicolas@dupeux.net>
2012-09-17 23:06:06 +0200

    Remove views

--- software/projects/ihm/urls.py
+++ software/projects/ihm/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bugone.views.home', name='home'),
-    url(r'^bugone/', include('web.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
