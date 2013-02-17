4b39358e5ab9cfbed63b8d2b0797a6809e2d2983
Nicolas Dupeux <nicolas@dupeux.net>
2013-02-17 18:12:52 +0100

    Initial import

---
+++ software/projects/shield1/main.c
@@ -0,0 +1,29 @@
+#include "bugOne.h"
+
+#include <stdlib.h>
+
+#include "avr_compat.h"
+#include "config.h"
+
+#include "apps.h"
+
+#include "const.h"
+#include "bandgap.h"
+#include "led.h"
+#include "photoresistor.h"
+#include "dht11.h"
+
+// Send datas every period seconds
+const uint8_t period = 10;
+
+// XXX: Theses structures belong to PROGMEM ...
+const photoresistor_cfg_s photoresistor_cfg = {3};
+
+application_t applications[] = {
+ {NULL,const_read,NULL,NULL},
+ {bandgap_init,bandgap_get,NULL,NULL},
+ {photoresistor_init,photoresistor_read,NULL,(void*)&photoresistor_cfg},
+ {NULL,dht11_temperature_read,NULL,NULL},
+ {NULL,dht11_humidity_read,NULL,NULL},
+ {NULL,NULL,NULL,NULL},
+};
