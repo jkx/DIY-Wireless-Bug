74b613cb49f75ff964485912574d7cf3653ea9c8
Nicolas Dupeux <nicolas@dupeux.net>
2014-01-26 16:31:09 +0100

    Add project for shield2
    shield 2 is a simple shield with only a 18b20.

---
+++ software/projects/shield2/main.c
@@ -0,0 +1,24 @@
+#include "bugOne.h"
+
+#include <stdlib.h>
+
+#include "avr_compat.h"
+#include "config.h"
+
+#include "apps.h"
+
+#include "const.h"
+#include "bandgap.h"
+#include "led.h"
+#include "ds18x20.h"
+
+// Send datas every period seconds
+const uint8_t period = 5;
+
+// XXX: Theses structures belong to PROGMEM ...
+application_t applications[] = {
+ {NULL,const_read,NULL,NULL},
+ {bandgap_init,bandgap_get,NULL,NULL},
+ {ds18x20_init,ds18x20_read,NULL,NULL},
+ {NULL,NULL,NULL,NULL},
+};
