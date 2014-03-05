bd936376ee15a498878dbff4b4f3455deff07fdb
Jkx <jkx@larsen-b.com>
2014-03-05 22:11:27 +0100

    Some cleanup..

--- software/projects/ds18b20/main.c
+++ software/projects/ds18b20/main.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 #include "avr_compat.h"
-#include "config.h"
 
 #include "apps.h"
 
 #include "led.h"
 #include "ds18x20.h"
 #include "dht11.h"
+#include "bandgap.h"
+
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
@@ -21,11 +22,11 @@
 
 
 // XXX: Theses structures belong to PROGMEM ...
-
 application_t applications[] = {
+  {bandgap_init,bandgap_get,NULL,NULL},
   {ds18x20_init,ds18x20_read,NULL,NULL},
-  {NULL,dht11_temperature_read,NULL,NULL},
-  {NULL,dht11_humidity_read,NULL,NULL},
+  //{NULL,dht11_temperature_read,NULL,NULL},
+  //{NULL,dht11_humidity_read,NULL,NULL},
   {NULL,NULL,NULL,NULL},
 };
 
@@ -40,37 +41,41 @@ void io_init()
 }
 
 
-int main(void) {
-  io_init();
-  bugone_init(applications);
+void io_clear()
+{
+  clr_output(DHT11_VCC);
+}
 
+
+
+// The watchdog wake us, what we do ?
+SIGNAL(WDT_vect) {
+  bugone_wakeup();
+}
+
+
+int main(void) {
   int r = 0;
   char buf[16];
+  uint8_t wake_up=10;
+
+
+  bugone_init(applications);
+  bugone_setup_watchdog(9);
   
-  while (1) {
-    //bugone_loop();
-
-    set_output(DHT11_VCC);
-    r = dht11_read();
-    sprintf(buf,"dht11_read=%d\r\n",r);
-    uart_putstr(buf);
-
-    r = dht11_humidity_val();
-    sprintf(buf,"hum=%d  ",r);
-    uart_putstr(buf);
-
-    r = dht11_temperature_val();
-    sprintf(buf,"tmp=%d\r\n",r);
-    uart_putstr(buf);
-
-    clr_output(DHT11_VCC);
-    delay_1s();
-    delay_1s();
-    delay_1s();
-    delay_1s();
-
-
-
+  while (1) 
+  {
+    wake_up++;
+    if (wake_up > 10)
+    {
+      led_blink2();
+      io_init();
+      delay_500ms();
+      bugone_send();
+      wake_up=0;
+    }
+  io_clear();
+  bugone_sleep();
   }
 }
 
