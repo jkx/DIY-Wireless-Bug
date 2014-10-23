9887e5f9f4c56f7d902f3f70802496c05bf574b8
Pierre-Henri Horrein <ph.horrein@telecom-bretagne.eu>
2014-10-24 00:19:37 +0200

    New miniaturized bugOne board
    Creating new "mini" bugOne board, with some added features:
       * MOSFET for complete poweroff of RMF12B
       * ISP moved further away from the shield
       * smaller LDO regulator (MCP1700). This might be changed in a future version
       * added a GND via near the antenna
    
    Next features:
       * holes to attach the board
       * jumper to short the MOSFET (in case a more usual sleep mode is desired)
       * add missing MOSFET control from the AVR
       * rotate all names

---
+++ hardware/bugOne/mini/bugOne_mini.cmd
@@ -0,0 +1,154 @@
+# Pin name action command file
+
+# Start of element Q1
+ChangePinName(Q1, G, G)
+ChangePinName(Q1, S, S)
+ChangePinName(Q1, D, D)
+
+# Start of element U3
+ChangePinName(U3, 4, TAB)
+ChangePinName(U3, 3, VOUT)
+ChangePinName(U3, 1, GND)
+ChangePinName(U3, 2, VIN)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 9, 9)
+ChangePinName(CONN1, 10, 10)
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 9, 9)
+ChangePinName(CONN2, 10, 10)
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN4
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 2, 2)
+ChangePinName(CONN4, 3, 3)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R0
+ChangePinName(R0, 1, 1)
+ChangePinName(R0, 2, 2)
+
+# Start of element F1
+ChangePinName(F1, 1, 1)
+ChangePinName(F1, 2, 2)
+
+# Start of element LED1
+ChangePinName(LED1, 2, K)
+ChangePinName(LED1, 1, A)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element LED2
+ChangePinName(LED2, 2, K)
+ChangePinName(LED2, 1, A)
+
+# Start of element CONN3
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 3, 3)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 14, nSEL)
+ChangePinName(U2, 13, SCK)
+ChangePinName(U2, 7, GND)
+ChangePinName(U2, 11, nINT)
+ChangePinName(U2, 10, GND)
+ChangePinName(U2, 9, VDD)
+ChangePinName(U2, 8, ANT)
+ChangePinName(U2, 6, nRES)
+ChangePinName(U2, 5, CLK)
+ChangePinName(U2, 4, DCLK)
+ChangePinName(U2, 3, FSK)
+ChangePinName(U2, 2, nIRQ)
+ChangePinName(U2, 1, SDO)
+ChangePinName(U2, 12, SDI)
+
+# Start of element CONN5
+ChangePinName(CONN5, 5, REST)
+ChangePinName(CONN5, 4, MOSI)
+ChangePinName(CONN5, 3, SCK)
+ChangePinName(CONN5, 2, VTG)
+ChangePinName(CONN5, 1, MISO)
+ChangePinName(CONN5, 6, GND)
+
+# Start of element U1
+ChangePinName(U1, 22, ADC7)
+ChangePinName(U1, 19, ADC6)
+ChangePinName(U1, 6, VCC)
+ChangePinName(U1, 5, GND)
+ChangePinName(U1, 11, "PD7 (PCINT23/AIN1)")
+ChangePinName(U1, 10, "PD6 (PCINT22/OC0A/AIN0)")
+ChangePinName(U1, 9, "PD5 (PCINT21/OC0B/T1)")
+ChangePinName(U1, 2, "PD4 (PCINT20/XCK/T0)")
+ChangePinName(U1, 1, "PD3 (PCINT19/OC2B/INT1)")
+ChangePinName(U1, 32, "PD2 (INT0/PCINT18)")
+ChangePinName(U1, 31, "PD1 (TXD/PCINT17)")
+ChangePinName(U1, 30, "PD0 (RXD/PCINT16)")
+ChangePinName(U1, 8, "PB7 (PCINT7/XTAL2/TOSC2)")
+ChangePinName(U1, 7, "PB6 (PCINT6/XTAL1/TOSC1)")
+ChangePinName(U1, 17, "PB5 (SCK/PCINT5)")
+ChangePinName(U1, 16, "PB4 (PCINT4/MISO)")
+ChangePinName(U1, 15, "PB3 (PCINT3/OC2A/MOSI)")
+ChangePinName(U1, 14, "PB2 (PCINT2/OC1B)")
+ChangePinName(U1, 13, "PB1 (PCINT1/OC1A)")
+ChangePinName(U1, 12, "PB0 (PCINT0/CLK0/ICP1)")
+ChangePinName(U1, 29, "(_RESET_/PCINT14) PC6")
+ChangePinName(U1, 28, "(ADC5/PCINT13) PC5")
+ChangePinName(U1, 27, "(ADC4/PCINT12) PC4")
+ChangePinName(U1, 26, "(ADC3/PCINT11) PC3")
+ChangePinName(U1, 25, "(ADC2/PCINT10) PC2")
+ChangePinName(U1, 24, "(ADC1/PCINT9) PC1")
+ChangePinName(U1, 23, "(ADC0/PCINT8) PC0")
+ChangePinName(U1, 18, AVCC)
+ChangePinName(U1, 20, AREF)
+ChangePinName(U1, 21, GND)
+ChangePinName(U1, 3, GND)
+ChangePinName(U1, 4, VCC)
