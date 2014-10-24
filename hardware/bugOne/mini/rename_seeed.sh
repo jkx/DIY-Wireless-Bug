9151048fe2917c5e7e5e1571210b840565360601
Pierre-Henri Horrein <ph.horrein@gmail.com>
2014-10-25 00:41:29 +0200

    Labellized and "gerberized" version of bugOne mini

---
+++ hardware/bugOne/mini/rename_seeed.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mv bugOne_mini.group1.gbr bugOne_mini.GBL
+mv bugOne_mini.bottomsilk.gbr bugOne_mini.GBO
+mv bugOne_mini.bottommask.gbr bugOne_mini.GBS
+mv bugOne_mini.outline.gbr bugOne_mini.GML
+mv bugOne_mini.top.gbr bugOne_mini.GTL
+mv bugOne_mini.topsilk.gbr bugOne_mini.GTO
+mv bugOne_mini.topmask.gbr bugOne_mini.GTS
+mv bugOne_mini.plated-drill.cnc bugOne_mini.TXT
+
+rm bugOne_mini.*.gbr bugOne_mini.*.cnc
