a78a0df5d496ad4a3f015b2b8a8c8278825e231a
Freki <frekilabs@gmail.com>
2014-10-30 00:11:37 +0100

    Adding SHT21 breakout board for repeater

---
+++ hardware/shields/sensor_shield/sht21_breakout/sht21_breakout.cmd
@@ -0,0 +1,29 @@
+# Pin name action command file
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 3, 3)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 3, 3)
+
+# Start of element U1
+ChangePinName(U1, 4, NC)
+ChangePinName(U1, 5, VCC)
+ChangePinName(U1, 6, SCL)
+ChangePinName(U1, 3, NC)
+ChangePinName(U1, 2, VSS)
+ChangePinName(U1, 1, SDA)
