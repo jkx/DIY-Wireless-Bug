85de18e0c88702af65a5c0c50627f9ce0885cd3d
Freki <frekilabs@gmail.com>
2014-10-29 23:40:43 +0100

    Adding a small power supply board for 1.5V battery operation
    The board is based on a MCP1640 low power regulator. It is compatible as a power header for bugOne mini board.

---
+++ hardware/shields/boost_battery/boost_battery.cmd
@@ -0,0 +1,49 @@
+# Pin name action command file
+
+# Start of element BAT1
+ChangePinName(BAT1, 1, 1)
+ChangePinName(BAT1, 2, 2)
+
+# Start of element ALIM2
+ChangePinName(ALIM2, 1, 1)
+ChangePinName(ALIM2, 4, 4)
+ChangePinName(ALIM2, 2, 2)
+ChangePinName(ALIM2, 3, 3)
+
+# Start of element ALIM1
+ChangePinName(ALIM1, 1, 1)
+ChangePinName(ALIM1, 4, 4)
+ChangePinName(ALIM1, 2, 2)
+ChangePinName(ALIM1, 3, 3)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 4, VFB)
+ChangePinName(U1, 5, VOUT)
+ChangePinName(U1, 6, VIN)
+ChangePinName(U1, 3, EN)
+ChangePinName(U1, 2, GND)
+ChangePinName(U1, 1, SW)
