f0d08600475b33d18756d2bd94ddc5075e9719cd
Pierre-Henri Horrein <ph.horrein@gmail.com>
2014-11-08 12:57:41 +0100

    Correction of shorts on battery supply

--- hardware/shields/boost_battery/boost_battery.cmd
+++ hardware/shields/boost_battery/boost_battery.cmd
@@ -1,5 +1,20 @@
 # Pin name action command file
 
+# Start of element J2
+ChangePinName(J2, 3, 3)
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
 # Start of element BAT1
 ChangePinName(BAT1, 1, 1)
 ChangePinName(BAT1, 2, 2)
