39f94536cd51eb72cd68522a23d0b1fc52eaca65
Pierre-Henri Horrein <freki@frekilabs.fr>
2016-10-10 22:02:16 +0200

    Improving apps
    Add types for applications (beta list)
    Add configurability on the duty cycle (all apps does not necessarily need to
    send at each wake up) to save on battery

--- software/core/apps.h
+++ software/core/apps.h
@@ -4,11 +4,30 @@
 #include <stdint.h>
 #include "network.h"
 
+enum {
+	TEMPERATURE_APP, // Temperature measurement (SHT21, DS18x20, LM35)
+	HUMIDITY_APP, // DHTX1, SHTX1
+	BATTERY_APP, // battery measurement (ADC)
+	BANDGAP_APP, // Bandgap measurement
+	EVENT_APP, // Apps which send an event (button pressed, threshold reached, ...)
+	SWITCH_APP, // Switch apps, supports turning on or off a switch (relay, ...)
+	LUM_APP, // Luminosity measurement
+ 	DIMMER_APP, // Dimmmer (DAC, LEDs, ...)
+};
+
+
+
 typedef struct {
 	void (*init)(void*);
 	int8_t (*get)(struct packet_t*);
 	void (*set)(struct packet_t*);
 	void *cfg;
+	// Type of data from previous enum
+	uint8_t type;
+	// Number of wdt interrupt before transmission
+	int sleeptime;
+	// Number of interrupt since last transmission
+	int current_time;
 } application_t;
 
 
